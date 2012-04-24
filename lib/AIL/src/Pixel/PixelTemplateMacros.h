20b1253a44d6a1d09ebaf61ff878f0b4f07ca721
Argoday <argoday@argoday.com>
2012-04-24 10:36:22 -0400

    Simple | Allow specific PixelTypes to be ignored in the explicit template instantiation macros

--- lib/AIL/src/Pixel/PixelTemplateMacros.h
+++ lib/AIL/src/Pixel/PixelTemplateMacros.h
@@ -6,7 +6,7 @@
 #include "PixelRGB.h"
 #include "PixelYUV.h"
 
-#define PIXEL_TYPE_1 Pixel::PixelYb
+#define PIXEL_TYPE_1 Pixel::PixelYb1
 #define PIXEL_TYPE_2 Pixel::PixelYi1u
 #define PIXEL_TYPE_3 Pixel::PixelRGBi1u
 #define PIXEL_TYPE_4 Pixel::PixelRGBf8
@@ -15,9 +15,8 @@
 #define PIXEL_TYPE_F_1 Pixel::PixelRGBf8
 #define PIXEL_TYPE_F_2 Pixel::PixelYUVf8
 
-
 #define CREATE_PIXEL_TEMPLATE_CALLS(x) \
-	template class x<Pixel::PixelYb>; \
+	template class x<Pixel::PixelYb1>; \
 	template class x<Pixel::PixelYi1u>; \
 	\
 	template class x<Pixel::PixelRGBi1u>; \
@@ -26,25 +25,25 @@
 	template class x<Pixel::PixelYUVf8>;
 
 #define CREATE_PIXEL_TEMPLATE_CALLS_2(x) \
-	template class x<Pixel::PixelYb,   Pixel::PixelYb  >; \
+	template class x<Pixel::PixelYb,   Pixel::PixelYb1  >; \
 	template class x<Pixel::PixelYb,   Pixel::PixelYi1u>; \
 	template class x<Pixel::PixelYb,   Pixel::PixelRGBi1u >; \
 	template class x<Pixel::PixelYb,   Pixel::PixelRGBf8  >; \
 	template class x<Pixel::PixelYb,   Pixel::PixelYUVf8  >; \
 	\
-	template class x<Pixel::PixelRGBi1u,Pixel::PixelYb  >; \
+	template class x<Pixel::PixelRGBi1u,Pixel::PixelYb1  >; \
 	template class x<Pixel::PixelRGBi1u,Pixel::PixelYi1u>; \
 	template class x<Pixel::PixelRGBi1u,Pixel::PixelRGBi1u >; \
 	template class x<Pixel::PixelRGBi1u,Pixel::PixelRGBf8  >; \
 	template class x<Pixel::PixelRGBi1u,Pixel::PixelYUVf8  >; \
 	\
-	template class x<Pixel::PixelRGBf8,Pixel::PixelYb  >; \
+	template class x<Pixel::PixelRGBf8,Pixel::PixelYb1  >; \
 	template class x<Pixel::PixelRGBf8,Pixel::PixelYi1u>; \
 	template class x<Pixel::PixelRGBf8,Pixel::PixelRGBi1u >; \
 	template class x<Pixel::PixelRGBf8,Pixel::PixelRGBf8  >; \
 	template class x<Pixel::PixelRGBf8,Pixel::PixelYUVf8  >; \
 	\
-	template class x<Pixel::PixelYUVf8,Pixel::PixelYb  >; \
+	template class x<Pixel::PixelYUVf8,Pixel::PixelYb1  >; \
 	template class x<Pixel::PixelYUVf8,Pixel::PixelYi1u>; \
 	template class x<Pixel::PixelYUVf8,Pixel::PixelRGBi1u >; \
 	template class x<Pixel::PixelYUVf8,Pixel::PixelRGBf8  >; \
@@ -57,7 +56,7 @@
 
 
 #define TEMPLATE_PIXEL_TYPE_2_DEF_0(x,y) \
-	x Pixel::PixelYb  y ; \
+	x Pixel::PixelYb1  y ; \
 	x Pixel::PixelYi1u  y ; \
 	\
 	x Pixel::PixelRGBi1u y ; \
@@ -72,7 +71,7 @@
 
 
 #define TEMPLATE_PIXEL_TYPE_2_DEF_1(x,y,z,a) \
-	x ( y Pixel::PixelYb   z ) a ; \
+	x ( y Pixel::PixelYb1   z ) a ; \
 	x ( y Pixel::PixelYi1u z ) a ; \
 	\
 	x ( y Pixel::PixelRGBi1u  z ) a ; \
