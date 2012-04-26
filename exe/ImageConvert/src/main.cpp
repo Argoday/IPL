cee6e0ee46d31a0a35d871e4e24a7ad71acbe83b
Argoday <argoday@argoday.com>
2012-04-26 01:33:54 -0400

    Close #28 | Change ImageConvert from a Qt project to a normal project

--- exe/ImageConvert/src/main.cpp
+++ exe/ImageConvert/src/main.cpp
@@ -1,49 +1,21 @@
 
-#include <String/AString.h>
-
-#include <qcoreapplication.h>
-#include <qtimer.h>
-#include <qstringlist.h>
-
-#include <sstream>
-#include <iostream>
-#include <memory>
-#include <stdio.h>
-#include <vector>
-
 #include <Image/Image_IO.h>
 
-#include "MainObject.h"
+#include <string>
+#include <iostream>
 
-int main(int argc, char *argv[]){
-
-	QCoreApplication a(argc, argv);
- 
-	MainObject g;
-	QTimer::singleShot(0, &g, SLOT(startApplication()));
- 
-	return a.exec();
-}
-
-
-void realMain(const QStringList &args,const QString & filePath){
-	if((args.length()>4)||(args.length()<2)){
-		return;
+int main(int argc, char * argv[]){
+	if(argc!=3){
+		std::cout<<"Usage example: ./ImageConvert.exe A.bmp A.png"<<std::endl;
+		return 0;
 	}
-	bool verbose=false;
-	if(args.length()==4){
-		if(args[3].toStdString()=="verbose"){
-			verbose=true;
-		}else{
-			return;
-		}
-	}
-	std::string fromFileName = args[1].toStdString();
-	std::string toFileName   = args[2].toStdString();
+	std::string fromFileName = argv[1];
+	std::string toFileName   = argv[2];
 	Image::Image<Pixel::PixelRGBf8> image = Image::IO::read<Pixel::PixelRGBf8>(nullptr,fromFileName);
 	if(image.getHeight()==0){
 		std::cout<<"Error Reading File - received image of zero height"<<std::endl;
 	}else{
 		Image::IO::write(image,toFileName);
 	}
+	return 1;
 }
