74f1a9e228368d37eadb2c98908b9a72faddce44
Argoday <argoday@argoday.com>
2012-04-26 01:41:00 -0400

    Cleanup |

--- exe/MediaPlayer/src/main.cpp
+++ exe/MediaPlayer/src/main.cpp
@@ -1,20 +1,12 @@
 
 #include "MediaPlayer.h"
-
 #include <QtGui/QApplication>
-
 #include <Data/DataManager.h>
 
 int main(int argc, char *argv[]){
-
 	Data::DataManager dataManager;
-
 	QApplication a(argc, argv);
 	MediaPlayer w(&dataManager);
-
 	w.show();
-
-	I4 ret = a.exec();
-
-	return ret;
+	return a.exec();
 }
