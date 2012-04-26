d60bd479b843df74825eb2e97ae7749c274fc040
Argoday <argoday@argoday.com>
2012-04-25 20:57:02 -0400

    Simple | Use _d pattern

--- lib/ACore/platform/windows/src/Thread/Semaphore.cpp
+++ lib/ACore/platform/windows/src/Thread/Semaphore.cpp
@@ -1,23 +1,38 @@
 
 #include "Semaphore.h"
 
+#include <concurrent_queue.h>
+
 namespace Thread {
 
+class Semaphore::Semaphore_d {
+	public:
+		volatile I8 count;
+
+		Concurrency::concurrent_queue<Concurrency::Context*> waitingContexts;
+		Concurrency::critical_section criticalSection;
+};
+
+
 Semaphore::Semaphore(I8 capacity)
-	:count(capacity)
+	:_this(new Semaphore_d)
 {
+	_this->count = capacity;
 }
 
+Semaphore::~Semaphore(){
+	delete _this;
+}
 
 void Semaphore::acquire(){
 	I8 tempCount;
 	{
-		Concurrency::critical_section::scoped_lock lock(criticalSection);
-		--count;
-		tempCount = count;
+		Concurrency::critical_section::scoped_lock lock(_this->criticalSection);
+		--_this->count;
+		tempCount = _this->count;
 	}
 	if(tempCount < 0){
-		waitingContexts.push(Concurrency::Context::CurrentContext());
+		_this->waitingContexts.push(Concurrency::Context::CurrentContext());
 		Concurrency::Context::Block();
 	}
 }
@@ -25,14 +40,14 @@ void Semaphore::acquire(){
 void Semaphore::release(){
 	I8 tempCount;
 	{
-		Concurrency::critical_section::scoped_lock lock(criticalSection);
-		++count;
-		tempCount = count;
+		Concurrency::critical_section::scoped_lock lock(_this->criticalSection);
+		++_this->count;
+		tempCount = _this->count;
 	}
 	if(tempCount <= 0){
 		Concurrency::Context * waiting = nullptr;
 		for(;;){
-			if(waitingContexts.try_pop(waiting)==true){
+			if(_this->waitingContexts.try_pop(waiting)==true){
 				waiting->Unblock();
 				break;
 			}
--- lib/ACore/platform/windows/src/Thread/Semaphore.h
+++ lib/ACore/platform/windows/src/Thread/Semaphore.h
@@ -5,23 +5,20 @@
 #include "ACore.h"
 #include <Data/DataTypes.h>
 
-#include <concurrent_queue.h>
-
 namespace Thread {
 
 class ACORE_DLL_EXPORT Semaphore {
 	public:
 	   explicit Semaphore(I8 capacity);
 
+	   ~Semaphore();
+
 	   void acquire();
 	   void release();
 
 	private:
-
-	   volatile I8 count;
-	   
-	   Concurrency::concurrent_queue<Concurrency::Context*> waitingContexts;
-	   Concurrency::critical_section criticalSection;
+		class Semaphore_d;
+		Semaphore_d * const _this;
 };
 
 }
