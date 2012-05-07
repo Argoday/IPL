0864dd49e21b589a72cb0d713685e6b77570a2a8
Argoday <argoday@argoday.com>
2012-05-07 09:12:06 -0400

    Work on #25 | Add non-simpleView cases

--- lib/AIL/src/Algorithm/BaseAlgorithm1xH_cpp.h
+++ lib/AIL/src/Algorithm/BaseAlgorithm1xH_cpp.h
@@ -17,44 +17,95 @@ template <
 	Image::ImageView<PixelDataType> & dstImage,
 	const ParameterType & parameter)
 {
-	if((srcImage.isSimpleView()==false)||(dstImage.isSimpleView()==false)){return;} // TODO: implement the other cases
 	if(srcImage.getSize()!=dstImage.getSize()){return;}
 
-	const auto & srcImageHeight = srcImage.getHeight();
-	const auto & srcImageWidth  = srcImage.getWidth();
+	if((srcImage.isSimpleView()==true)||(dstImage.isSimpleView()==true)){
+		const auto & srcImageHeight = srcImage.getHeight();
+		const auto & srcImageWidth  = srcImage.getWidth();
 
-	const auto & yOffset      = parameter.yOffset;
-	const auto & filterHeight = parameter.filterHeight;
+		const auto & yOffset      = parameter.yOffset;
+		const auto & filterHeight = parameter.filterHeight;
 
-	auto srcImageHeight_filterHeightOffset = srcImageHeight-(filterHeight-yOffset);
+		auto srcImageHeight_filterHeightOffset = srcImageHeight - (filterHeight - yOffset - 1);
 
-	auto srcImageDataPtr = srcImage.getDataPtr();
-	auto dstImageDataPtr = dstImage.getDataPtr();
-	auto dstImageDataPtrRowsEnd = dstImage.getDataPtr() + (srcImageHeight_filterHeightOffset)*srcImageWidth;
+		auto srcImageDataPtr = srcImage.getDataPtr();
+		auto dstImageDataPtr = dstImage.getDataPtr();
+		auto dstImageDataPtrRowsEnd = dstImage.getDataPtr() + (srcImageHeight_filterHeightOffset)*srcImageWidth;
 
-	I4 y = 0;
-	I4 x = 0;
+		I4 y = 0;
+		I4 x = 0;
 
-	//First Rows
-	for(;y<yOffset;++y){
-		for (x=0; x<srcImageWidth; ++x){
-			AlgorithmType::process(dstImageDataPtr,srcImage,parameter,x,y);
-			++dstImageDataPtr;
+		//First Rows
+		for(;y<yOffset;++y){
+			for (x=0; x<srcImageWidth; ++x){
+				AlgorithmType::process(dstImageDataPtr,srcImage,parameter,x,y);
+				++dstImageDataPtr;
+			}
 		}
-	}
 
-	//Inside Rows
-	for (;dstImageDataPtr!=dstImageDataPtrRowsEnd;){
-		AlgorithmType::process(dstImageDataPtr,srcImageDataPtr,parameter);
-		++dstImageDataPtr;
-		++srcImageDataPtr;
-	}
-
-	//Last Rows
-	for(y=srcImageHeight_filterHeightOffset;y<srcImageHeight;++y){
-		for (x=0; x<srcImageWidth; ++x){
-			AlgorithmType::process(dstImageDataPtr,srcImage,parameter,x,y);
+		//Inside Rows
+		for (;dstImageDataPtr!=dstImageDataPtrRowsEnd;){
+			AlgorithmType::process(dstImageDataPtr,srcImageDataPtr,parameter);
 			++dstImageDataPtr;
+			++srcImageDataPtr;
+		}
+
+		//Last Rows
+		for(y=srcImageHeight_filterHeightOffset;y<srcImageHeight;++y){
+			for (x=0; x<srcImageWidth; ++x){
+				AlgorithmType::process(dstImageDataPtr,srcImage,parameter,x,y);
+				++dstImageDataPtr;
+			}
+		}
+	}else{
+		const auto & srcImageHeight = srcImage.getHeight();
+		const auto & srcImageWidth  = srcImage.getWidth();
+
+		const auto & yOffset      = parameter.yOffset;
+		const auto & filterHeight = parameter.filterHeight;
+
+		auto srcImageHeight_filterHeightOffset = srcImageHeight - (filterHeight - yOffset - 1);
+
+		auto srcImageDataPtr = srcImage.getDataPtr();
+		auto dstImageDataPtr = dstImage.getDataPtr();
+
+		I4 y = 0;
+		I4 x = 0;
+
+		if(srcImage.hasYbeginSection()==true){
+			//First Rows - Inside Section
+			for(y=0;y<yOffset;++y){
+				for(x=0;x<srcImageWidth;++x){
+					AlgorithmType::process(dstImageDataPtr,srcImage,parameter,x,y);
+					++dstImageDataPtr;
+				}
+				dstImageDataPtr+=dstImage.getNumPixelsBetweenRows();
+			}
+		}else{
+			dstImageDataPtr+=yOffset*srcImage.getStride();
+		}
+
+		//Inside Rows
+		for(y=yOffset;y<srcImageHeight_filterHeightOffset;++y){
+			auto dstImageDataPtrRowEnd = dstImageDataPtr + srcImageWidth;
+			for(;dstImageDataPtr!=dstImageDataPtrRowEnd;){
+				AlgorithmType::process(dstImageDataPtr,srcImageDataPtr,parameter);
+				++dstImageDataPtr;
+				++srcImageDataPtr;
+			}
+			dstImageDataPtr+=dstImage.getNumPixelsBetweenRows();
+			srcImageDataPtr+=srcImage.getNumPixelsBetweenRows();
+		}
+
+		if(srcImage.hasYendSection()==true){
+			//Last Rows
+			for(y=srcImageHeight_filterHeightOffset;y<srcImageHeight;++y){
+				for(x=0;x<srcImageWidth;++x){
+					AlgorithmType::process(dstImageDataPtr,srcImage,parameter,x,y);
+					++dstImageDataPtr;
+				}
+				dstImageDataPtr+=dstImage.getNumPixelsBetweenRows();
+			}
 		}
 	}
 
--- lib/AIL/src/Algorithm/BaseAlgorithm3x3_cpp.h
+++ lib/AIL/src/Algorithm/BaseAlgorithm3x3_cpp.h
@@ -14,91 +14,252 @@ template <
 	Image::ImageView<PixelDataType> & dstImage)
 {
 	if(srcImage.getSize()!=dstImage.getSize()){return;}
-	if((srcImage.isSimpleView()==false)||(dstImage.isSimpleView()==false)){return;} // TODO: implement the other cases
 
-	auto srcImageHeightm1 = srcImage.getHeight()-1;
+	if((srcImage.isSimpleView()==true)&&(dstImage.isSimpleView()==true)){
+		auto srcImageHeightm1 = srcImage.getHeight()-1;
 
-	const auto & srcImageWidth   = srcImage.getWidth();
-	      auto   srcImageWidthm1 = srcImage.getWidth()-1;
-	      auto   srcImageWidthp1 = srcImage.getWidth()+1;
+		const auto & srcImageWidth   = srcImage.getWidth();
+			  auto   srcImageWidthm1 = srcImage.getWidth()-1;
+			  auto   srcImageWidthp1 = srcImage.getWidth()+1;
 
-	auto srcImageDataPtr = srcImage.getDataPtr();
-	auto dstImageDataPtr = dstImage.getDataPtr();
+		auto srcImageDataPtr = srcImage.getDataPtr();
+		auto dstImageDataPtr = dstImage.getDataPtr();
 	
-	I4 y = 0; // TODO: Write these loops to get rid of x and y ... and make sure that the complier still unrolls the loops
-	I4 x = 0;
+		I4 y = 0; // TODO: Write these loops to get rid of x and y ... and make sure that the complier still unrolls the loops
+		I4 x = 0;
 
-	//First Row - First Pixel
-	AlgorithmType::process(dstImageDataPtr,srcImageDataPtr              ,srcImageDataPtr              ,srcImageDataPtr+1              ,
-	                                       srcImageDataPtr              ,srcImageDataPtr              ,srcImageDataPtr+1              ,
-	                                       srcImageDataPtr+srcImageWidth,srcImageDataPtr+srcImageWidth,srcImageDataPtr+srcImageWidthp1);
-	++dstImageDataPtr;
-	++srcImageDataPtr;
-
-	//First Row - Inside Pixels
-	for (x=1; x<srcImageWidthm1; ++x){
-		AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr+1              ,
-											   srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr+1              ,
-											   srcImageDataPtr+srcImageWidthm1,srcImageDataPtr+srcImageWidth,srcImageDataPtr+srcImageWidthp1);
-		++dstImageDataPtr;
-		++srcImageDataPtr;
-	}
-	//First Row - Last Pixel
-	AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr              ,
-										   srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr              ,
-										   srcImageDataPtr+srcImageWidthm1,srcImageDataPtr+srcImageWidth,srcImageDataPtr+srcImageWidth);
-	++dstImageDataPtr;
-	++srcImageDataPtr;
-
-	for (y=1; y<srcImageHeightm1; ++y){	
-		//Inside Row - First Pixel
-		x=0;
-		AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageWidth,srcImageDataPtr-srcImageWidth,srcImageDataPtr-srcImageWidthm1,
+		//First Row - First Pixel
+		AlgorithmType::process(dstImageDataPtr,srcImageDataPtr              ,srcImageDataPtr              ,srcImageDataPtr+1              ,
 											   srcImageDataPtr              ,srcImageDataPtr              ,srcImageDataPtr+1              ,
 											   srcImageDataPtr+srcImageWidth,srcImageDataPtr+srcImageWidth,srcImageDataPtr+srcImageWidthp1);
 		++dstImageDataPtr;
 		++srcImageDataPtr;
-		//Inside Row - Inside Pixels
-		for (x=1; x<srcImageWidthm1; ++x){
-			AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageWidthp1,srcImageDataPtr-srcImageWidth,srcImageDataPtr-srcImageWidthm1,
+
+		//First Row - Inside Pixels
+		for(x=1; x<srcImageWidthm1; ++x){
+			AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr+1              ,
 												   srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr+1              ,
 												   srcImageDataPtr+srcImageWidthm1,srcImageDataPtr+srcImageWidth,srcImageDataPtr+srcImageWidthp1);
 			++dstImageDataPtr;
 			++srcImageDataPtr;
 		}
-		//Inside Row - Last Pixel
-		AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageWidthp1,srcImageDataPtr-srcImageWidth,srcImageDataPtr-srcImageWidth,
+		//First Row - Last Pixel
+		AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr              ,
 											   srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr              ,
 											   srcImageDataPtr+srcImageWidthm1,srcImageDataPtr+srcImageWidth,srcImageDataPtr+srcImageWidth);
 		++dstImageDataPtr;
 		++srcImageDataPtr;
-	}
-	x=0;
 
-	//Last Row - First pixel
-	AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageWidth,srcImageDataPtr-srcImageWidth,srcImageDataPtr-srcImageWidthm1,
-										   srcImageDataPtr              ,srcImageDataPtr              ,srcImageDataPtr+1              ,
-										   srcImageDataPtr              ,srcImageDataPtr              ,srcImageDataPtr+1              );
-	++dstImageDataPtr;
-	++srcImageDataPtr;
+		for(y=1; y<srcImageHeightm1; ++y){	
+			//Inside Row - First Pixel
+			AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageWidth,srcImageDataPtr-srcImageWidth,srcImageDataPtr-srcImageWidthm1,
+												   srcImageDataPtr              ,srcImageDataPtr              ,srcImageDataPtr+1              ,
+												   srcImageDataPtr+srcImageWidth,srcImageDataPtr+srcImageWidth,srcImageDataPtr+srcImageWidthp1);
+			++dstImageDataPtr;
+			++srcImageDataPtr;
+			//Inside Row - Inside Pixels
+			for(x=1; x<srcImageWidthm1; ++x){
+				AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageWidthp1,srcImageDataPtr-srcImageWidth,srcImageDataPtr-srcImageWidthm1,
+													   srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr+1              ,
+													   srcImageDataPtr+srcImageWidthm1,srcImageDataPtr+srcImageWidth,srcImageDataPtr+srcImageWidthp1);
+				++dstImageDataPtr;
+				++srcImageDataPtr;
+			}
+			//Inside Row - Last Pixel
+			AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageWidthp1,srcImageDataPtr-srcImageWidth,srcImageDataPtr-srcImageWidth,
+												   srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr              ,
+												   srcImageDataPtr+srcImageWidthm1,srcImageDataPtr+srcImageWidth,srcImageDataPtr+srcImageWidth);
+			++dstImageDataPtr;
+			++srcImageDataPtr;
+		}
 
-	//Last Row - Inside Pixels
-	for (x=1; x<srcImageWidthm1; ++x){
-		AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageWidthp1,srcImageDataPtr-srcImageWidth,srcImageDataPtr-srcImageWidthm1,
-											   srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr+1              ,
-											   srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr+1              );
+		//Last Row - First pixel
+		AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageWidth,srcImageDataPtr-srcImageWidth,srcImageDataPtr-srcImageWidthm1,
+											   srcImageDataPtr              ,srcImageDataPtr              ,srcImageDataPtr+1              ,
+											   srcImageDataPtr              ,srcImageDataPtr              ,srcImageDataPtr+1              );
 		++dstImageDataPtr;
 		++srcImageDataPtr;
+
+		//Last Row - Inside Pixels
+		for(x=1; x<srcImageWidthm1; ++x){
+			AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageWidthp1,srcImageDataPtr-srcImageWidth,srcImageDataPtr-srcImageWidthm1,
+												   srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr+1              ,
+												   srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr+1              );
+			++dstImageDataPtr;
+			++srcImageDataPtr;
+		}
+
+		//Last Row - Last Pixel
+		AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageWidthp1,srcImageDataPtr-srcImageWidth,srcImageDataPtr-srcImageWidth,
+											   srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr              ,
+											   srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr              );
+
+	}else{
+		auto srcImageHeightm1 = srcImage.getHeight()-1;
+
+		const auto & srcImageWidth   = srcImage.getWidth();
+			  auto   srcImageWidthm1 = srcImage.getWidth()-1;
+			  auto   srcImageWidthp1 = srcImage.getWidth()+1;
+
+		const auto & srcImageStride   = srcImage.getStride();
+			  auto   srcImageStridem1 = srcImage.getStride()-1;
+			  auto   srcImageStridep1 = srcImage.getStride()+1;
+
+		auto dstImageDataPtr = dstImage.getDataPtr();
+		auto srcImageDataPtr = srcImage.getDataPtr();
+	
+		I4 y = 0; // TODO: Write these loops to get rid of x and y ... and make sure that the complier still unrolls the loops
+		I4 x = 0;
+
+		if(srcImage.hasYbeginSection()==true){
+			if(srcImage.hasXbeginSection()==true){
+				//First Row - First Pixel
+				AlgorithmType::process(dstImageDataPtr,srcImageDataPtr               ,srcImageDataPtr               ,srcImageDataPtr+1              ,
+													   srcImageDataPtr               ,srcImageDataPtr               ,srcImageDataPtr+1              ,
+													   srcImageDataPtr+srcImageStride,srcImageDataPtr+srcImageStride,srcImageDataPtr+srcImageStridep1);
+			}
+			++dstImageDataPtr;
+			++srcImageDataPtr;
+
+			//First Row - Inside Pixels
+			for(x=1;x<srcImageWidthm1;++x){
+				AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-1               ,srcImageDataPtr               ,srcImageDataPtr+1              ,
+													   srcImageDataPtr-1               ,srcImageDataPtr               ,srcImageDataPtr+1              ,
+													   srcImageDataPtr+srcImageStridem1,srcImageDataPtr+srcImageStride,srcImageDataPtr+srcImageStridep1);
+				++dstImageDataPtr;
+				++srcImageDataPtr;
+			}
+
+			if(srcImage.hasXendSection()==true){
+				//First Row - Last Pixel
+				AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-1               ,srcImageDataPtr               ,srcImageDataPtr              ,
+													   srcImageDataPtr-1               ,srcImageDataPtr               ,srcImageDataPtr              ,
+													   srcImageDataPtr+srcImageStridem1,srcImageDataPtr+srcImageStride,srcImageDataPtr+srcImageStride);
+			}
+			++dstImageDataPtr;
+			++srcImageDataPtr;
+		}else{
+			dstImageDataPtr+=srcImageWidth;
+			srcImageDataPtr+=srcImageWidth;
+		}
+		dstImageDataPtr+=dstImage.getNumPixelsBetweenRows();
+		srcImageDataPtr+=srcImage.getNumPixelsBetweenRows();
+
+		if((srcImage.hasXbeginSection()==false)&&(srcImage.hasXendSection()==false)){
+			for(y=1;y<srcImageHeightm1;++y){
+				++dstImageDataPtr;
+				++srcImageDataPtr;
+				//Inside Row - Inside Pixels
+				for (x=1; x<srcImageWidthm1; ++x){
+					AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageStridep1,srcImageDataPtr-srcImageStride,srcImageDataPtr-srcImageStridem1,
+														   srcImageDataPtr-1               ,srcImageDataPtr               ,srcImageDataPtr+1              ,
+														   srcImageDataPtr+srcImageStridem1,srcImageDataPtr+srcImageStride,srcImageDataPtr+srcImageStridep1);
+					++dstImageDataPtr;
+					++srcImageDataPtr;
+				}
+				++dstImageDataPtr;
+				++srcImageDataPtr;
+				dstImageDataPtr+=dstImage.getNumPixelsBetweenRows();
+				srcImageDataPtr+=srcImage.getNumPixelsBetweenRows();
+			}
+		}else if((srcImage.hasXbeginSection()==true)&&(srcImage.hasXendSection()==false)){
+			for(y=1; y<srcImageHeightm1; ++y){	
+				//Inside Row - First Pixel
+				AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageStride,srcImageDataPtr-srcImageStride,srcImageDataPtr-srcImageStridem1,
+													   srcImageDataPtr               ,srcImageDataPtr               ,srcImageDataPtr+1              ,
+													   srcImageDataPtr+srcImageStride,srcImageDataPtr+srcImageStride,srcImageDataPtr+srcImageStridep1);
+				++dstImageDataPtr;
+				++srcImageDataPtr;
+				//Inside Row - Inside Pixels
+				for(x=1; x<srcImageWidthm1; ++x){
+					AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageStridep1,srcImageDataPtr-srcImageStride,srcImageDataPtr-srcImageStridem1,
+														   srcImageDataPtr-1               ,srcImageDataPtr               ,srcImageDataPtr+1              ,
+														   srcImageDataPtr+srcImageStridem1,srcImageDataPtr+srcImageStride,srcImageDataPtr+srcImageStridep1);
+					++dstImageDataPtr;
+					++srcImageDataPtr;
+				}
+				++dstImageDataPtr;
+				++srcImageDataPtr;
+				dstImageDataPtr+=dstImage.getNumPixelsBetweenRows();
+				srcImageDataPtr+=srcImage.getNumPixelsBetweenRows();
+			}
+		}else if((srcImage.hasXbeginSection()==false)&&(srcImage.hasXendSection()==true)){
+			for(y=1; y<srcImageHeightm1; ++y){	
+				++dstImageDataPtr;
+				++srcImageDataPtr;
+				//Inside Row - Inside Pixels
+				for(x=1; x<srcImageWidthm1; ++x){
+					AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageStridep1,srcImageDataPtr-srcImageStride,srcImageDataPtr-srcImageStridem1,
+														   srcImageDataPtr-1               ,srcImageDataPtr               ,srcImageDataPtr+1              ,
+														   srcImageDataPtr+srcImageStridem1,srcImageDataPtr+srcImageStride,srcImageDataPtr+srcImageStridep1);
+					++dstImageDataPtr;
+					++srcImageDataPtr;
+				}
+				//Inside Row - Last Pixel
+				AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageStridep1,srcImageDataPtr-srcImageStride,srcImageDataPtr-srcImageStride,
+													   srcImageDataPtr-1               ,srcImageDataPtr               ,srcImageDataPtr              ,
+													   srcImageDataPtr+srcImageStridem1,srcImageDataPtr+srcImageStride,srcImageDataPtr+srcImageStride);
+				++dstImageDataPtr;
+				++srcImageDataPtr;
+				dstImageDataPtr+=dstImage.getNumPixelsBetweenRows();
+				srcImageDataPtr+=srcImage.getNumPixelsBetweenRows();
+			}
+		}else{
+			for(y=1; y<srcImageHeightm1; ++y){	
+				//Inside Row - First Pixel
+				AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageStride,srcImageDataPtr-srcImageStride,srcImageDataPtr-srcImageStridem1,
+													   srcImageDataPtr               ,srcImageDataPtr               ,srcImageDataPtr+1              ,
+													   srcImageDataPtr+srcImageStride,srcImageDataPtr+srcImageStride,srcImageDataPtr+srcImageStridep1);
+				++dstImageDataPtr;
+				++srcImageDataPtr;
+				//Inside Row - Inside Pixels
+				for(x=1; x<srcImageWidthm1; ++x){
+					AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageStridep1,srcImageDataPtr-srcImageStride,srcImageDataPtr-srcImageStridem1,
+														   srcImageDataPtr-1               ,srcImageDataPtr               ,srcImageDataPtr+1              ,
+														   srcImageDataPtr+srcImageStridem1,srcImageDataPtr+srcImageStride,srcImageDataPtr+srcImageStridep1);
+					++dstImageDataPtr;
+					++srcImageDataPtr;
+				}
+				//Inside Row - Last Pixel
+				AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageStridep1,srcImageDataPtr-srcImageStride,srcImageDataPtr-srcImageStride,
+													   srcImageDataPtr-1               ,srcImageDataPtr               ,srcImageDataPtr              ,
+													   srcImageDataPtr+srcImageStridem1,srcImageDataPtr+srcImageStride,srcImageDataPtr+srcImageStride);
+				++dstImageDataPtr;
+				++srcImageDataPtr;
+				dstImageDataPtr+=dstImage.getNumPixelsBetweenRows();
+				srcImageDataPtr+=srcImage.getNumPixelsBetweenRows();
+			}
+		}
+
+		if(srcImage.hasYendSection()==true){
+			if(srcImage.hasXbeginSection()==true){
+				//Last Row - First pixel
+				AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageStride,srcImageDataPtr-srcImageStride,srcImageDataPtr-srcImageStridem1,
+														srcImageDataPtr              ,srcImageDataPtr               ,srcImageDataPtr+1               ,
+														srcImageDataPtr              ,srcImageDataPtr               ,srcImageDataPtr+1               );
+			}
+			++dstImageDataPtr;
+			++srcImageDataPtr;
+		
+			//Last Row - Inside Pixels
+			for (x=1; x<srcImageWidthm1; ++x){
+				AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageStridep1,srcImageDataPtr-srcImageStride,srcImageDataPtr-srcImageStridem1,
+													   srcImageDataPtr-1               ,srcImageDataPtr              ,srcImageDataPtr+1               ,
+													   srcImageDataPtr-1               ,srcImageDataPtr              ,srcImageDataPtr+1               );
+				++dstImageDataPtr;
+				++srcImageDataPtr;
+			}
+
+			if(srcImage.hasXendSection()==true){
+				//Last Row - Last Pixel
+				AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageStridep1,srcImageDataPtr-srcImageStride,srcImageDataPtr-srcImageStride,
+													   srcImageDataPtr-1               ,srcImageDataPtr              ,srcImageDataPtr              ,
+													   srcImageDataPtr-1               ,srcImageDataPtr              ,srcImageDataPtr              );
+			}
+		}
 	}
-
-	//Last Row - Last Pixel
-	AlgorithmType::process(dstImageDataPtr,srcImageDataPtr-srcImageWidthp1,srcImageDataPtr-srcImageWidth,srcImageDataPtr-srcImageWidth,
-										   srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr              ,
-										   srcImageDataPtr-1              ,srcImageDataPtr              ,srcImageDataPtr              );
-
 }
 
-
-};
+}
 
 #endif
