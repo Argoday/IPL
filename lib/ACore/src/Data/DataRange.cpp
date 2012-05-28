baaff05a425b7de62992ccaa7431a7d3e822f593
Argoday <argoday@argoday.com>
2012-05-28 17:08:21 -0400

    Simple | Update extended ranges to use the proper naming convention

--- lib/ACore/src/Data/DataRange.cpp
+++ lib/ACore/src/Data/DataRange.cpp
@@ -24,37 +24,37 @@ const I4  DataRange_I4::max =  2147483647i32;
 const I4u DataRange_I4u::min = 0x00000000ui32;
 const I4u DataRange_I4u::max = 0xFFFFFFFFui32;
 
-const I2  DataRange_I2_i1u::min = 0x00ui16;
-const I2  DataRange_I2_i1u::max = 0xFFui16;
+const I2  DataRange_I2_I1u::min = 0x00ui16;
+const I2  DataRange_I2_I1u::max = 0xFFui16;
 
-const I2  DataRange_I2_i1::min = -128i16;
-const I2  DataRange_I2_i1::max =  127i16;
+const I2  DataRange_I2_I1::min = -128i16;
+const I2  DataRange_I2_I1::max =  127i16;
 
-const I2u DataRange_I2u_i1u::min = 0x00ui16;
-const I2u DataRange_I2u_i1u::max = 0xFFui16;
+const I2u DataRange_I2u_I1u::min = 0x00ui16;
+const I2u DataRange_I2u_I1u::max = 0xFFui16;
 
-const I4  DataRange_I4_i1::min = -128i32;
-const I4  DataRange_I4_i1::max =  127i32;
+const I4  DataRange_I4_I1::min = -128i32;
+const I4  DataRange_I4_I1::max =  127i32;
 
-const I4  DataRange_I4_i1u::min = 0x00i32;
-const I4  DataRange_I4_i1u::max = 0xFFi32;
+const I4  DataRange_I4_I1u::min = 0x00i32;
+const I4  DataRange_I4_I1u::max = 0xFFi32;
 
-const I4u DataRange_I4u_i1u::min = 0x00ui32;
-const I4u DataRange_I4u_i1u::max = 0xFFui32;
+const I4u DataRange_I4u_I1u::min = 0x00ui32;
+const I4u DataRange_I4u_I1u::max = 0xFFui32;
 
-const I4  DataRange_I4_i2u::min = 0x0000i32;
-const I4  DataRange_I4_i2u::max = 0xFFFFi32;
+const I4  DataRange_I4_I2u::min = 0x0000i32;
+const I4  DataRange_I4_I2u::max = 0xFFFFi32;
 
-const I4  DataRange_I4_i2::min = -32768;
-const I4  DataRange_I4_i2::max =  32767i16;
+const I4  DataRange_I4_I2::min = -32768;
+const I4  DataRange_I4_I2::max =  32767i16;
 
-const I4u DataRange_I4u_i2u::min = 0x0000ui32;
-const I4u DataRange_I4u_i2u::max = 0xFFFFui32;
+const I4u DataRange_I4u_I2u::min = 0x0000ui32;
+const I4u DataRange_I4u_I2u::max = 0xFFFFui32;
 
-const F4 DataRange_F4_1::min = 0.0f;
-const F4 DataRange_F4_1::max = 1.0f;
+const F4  DataRange_F4_1::min = 0.0f;
+const F4  DataRange_F4_1::max = 1.0f;
 
-const F8 DataRange_F8_1::min = 0.0;
-const F8 DataRange_F8_1::max = 1.0;
+const F8  DataRange_F8_1::min = 0.0;
+const F8  DataRange_F8_1::max = 1.0;
 
 }
--- lib/ACore/src/Data/DataRange.h
+++ lib/ACore/src/Data/DataRange.h
@@ -19,7 +19,6 @@ template <
 
 		static FINLINE const NumberType & getMin() {return min;}
 		static FINLINE const NumberType & getMax() {return max;}
-
 };
 
 template <
@@ -32,33 +31,31 @@ template <
 
 		static FINLINE const NumberType & getMin() {return min;}
 		static FINLINE const NumberType & getMax() {return max;}
-
 };
 
-
-typedef DataRange_Base<Meta::Boolean> DataRange_B1;
-typedef DataRange_Base<I1 > DataRange_I1;
+typedef DataRange_Base<B1 > DataRange_B1 ;
+typedef DataRange_Base<I1 > DataRange_I1 ;
 typedef DataRange_Base<I1u> DataRange_I1u;
-typedef DataRange_Base<I2 > DataRange_I2;
+typedef DataRange_Base<I2 > DataRange_I2 ;
 typedef DataRange_Base<I2u> DataRange_I2u;
-typedef DataRange_Base<I4 > DataRange_I4;
+typedef DataRange_Base<I4 > DataRange_I4 ;
 typedef DataRange_Base<I4u> DataRange_I4u;
 
-typedef DataRange_BaseDepth<I2 ,1> DataRange_I2_i1  ;
-typedef DataRange_BaseDepth<I2 ,2> DataRange_I2_i1u ;
+typedef DataRange_BaseDepth<I2 ,1> DataRange_I2_I1  ;
+typedef DataRange_BaseDepth<I2 ,2> DataRange_I2_I1u ;
 
-typedef DataRange_BaseDepth<I2u,1> DataRange_I2u_i1u;
+typedef DataRange_BaseDepth<I2u,1> DataRange_I2u_I1u;
 
-typedef DataRange_BaseDepth<I4 ,1> DataRange_I4_i1  ;
-typedef DataRange_BaseDepth<I4 ,2> DataRange_I4_i1u ;
-typedef DataRange_BaseDepth<I4 ,3> DataRange_I4_i2  ;
-typedef DataRange_BaseDepth<I4 ,4> DataRange_I4_i2u ;
+typedef DataRange_BaseDepth<I4 ,1> DataRange_I4_I1  ;
+typedef DataRange_BaseDepth<I4 ,2> DataRange_I4_I1u ;
+typedef DataRange_BaseDepth<I4 ,3> DataRange_I4_I2  ;
+typedef DataRange_BaseDepth<I4 ,4> DataRange_I4_I2u ;
 
-typedef DataRange_BaseDepth<I4u,1> DataRange_I4u_i1u;
-typedef DataRange_BaseDepth<I4u,2> DataRange_I4u_i2u;
+typedef DataRange_BaseDepth<I4u,1> DataRange_I4u_I1u;
+typedef DataRange_BaseDepth<I4u,2> DataRange_I4u_I2u;
 
-typedef DataRange_BaseDepth<F4,1> DataRange_F4_1;
-typedef DataRange_BaseDepth<F8,1> DataRange_F8_1;
+typedef DataRange_BaseDepth<F4 ,1> DataRange_F4_1;
+typedef DataRange_BaseDepth<F8 ,1> DataRange_F8_1;
 
 }
 
