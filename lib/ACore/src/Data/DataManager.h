f6caa485643767486121380c3e3817d6db0d79f4
Argoday <argoday@argoday.com>
2012-05-28 16:40:38 -0400

    No-op | Spacing

--- lib/ACore/src/Data/DataManager.h
+++ lib/ACore/src/Data/DataManager.h
@@ -17,19 +17,19 @@ class ACORE_DLL_EXPORT DataManager {
 		~DataManager();
 
 	//Direct Memory Access
-		static void * const getMemory(const I4  &numBytes);
-		static void * const getMemory(const I4u &numBytes);
-		static void * const getMemory(const I8u &numBytes);
+		static void * const getMemory(const I4  & numBytes);
+		static void * const getMemory(const I4u & numBytes);
+		static void * const getMemory(const I8u & numBytes);
 		static void release(const void * const dataPtr);
 
 	//Memory Pool
-		void * const getMemoryFromPool(const I4  &numBytes);
-		void * const getMemoryFromPool(const I4u &numBytes);
-		void * const getMemoryFromPool(const I8u &numBytes);
+		void * const getMemoryFromPool(const I4  & numBytes);
+		void * const getMemoryFromPool(const I4u & numBytes);
+		void * const getMemoryFromPool(const I8u & numBytes);
 		B1 releaseFromPool(const void * const dataPtr);
-		B1 releaseFromPool(const void * const dataPtr,const I4  &numBytes);
-		B1 releaseFromPool(const void * const dataPtr,const I4u &numBytes);
-		B1 releaseFromPool(const void * const dataPtr,const I8u &numBytes);
+		B1 releaseFromPool(const void * const dataPtr,const I4  & numBytes);
+		B1 releaseFromPool(const void * const dataPtr,const I4u & numBytes);
+		B1 releaseFromPool(const void * const dataPtr,const I8u & numBytes);
 		
 		I8u releaseFreeFromPool();
 
